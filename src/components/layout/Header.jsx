9569115142683079ee543f488e3267273d07b0b1
bendrobinson1998 <benjidrobinson@gmail.com>
2022-12-29 19:10:18 +0000

    master

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
 <header className="text-gray-600 body-font">
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-    <img src={logo} width="100px"/>
-      <span className="ml-3 text-xl">Thomas Ices</span>
+    <img src={logo} width="100px" />
+      <span className="ml-3 text-xl mr-3">Thomas Ices</span>
     </a>
   
     <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Book Now
