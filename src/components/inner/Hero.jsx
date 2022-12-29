d86e485bf1934e858ebd3868db5e61330e9f0962
bendrobinson1998 <benjidrobinson@gmail.com>
2022-12-29 19:08:06 +0000

    style

--- src/components/inner/Hero.jsx
+++ src/components/inner/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     </div>
     <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Enjoy the authentic
-        <br className="hidden lg:inline-block" />ice cream man experience 🍦
+        <br className="hidden lg:inline-block" /> ice cream man experience 🍦
       </h1>
       <p className="mb-8 leading-relaxed">The family business started way back in the early 1950's when Mr Vincent Urso moved to the United Kingdom from his home country of Sicily.</p>
       <div className="flex justify-center">
